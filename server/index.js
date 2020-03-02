679483ab05c32d12f69832f9c5d83d9a727f6d8a
Roman <doroshroman27@gmail.com>
2020-03-02 19:19:46 +0200

    added nedb support

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Datastore = require('nedb');
 const app = express();
 const path = require('path');
 const fs = require('fs');
@@ -11,59 +12,21 @@ app.use('/iss',express.static(path.join(__dirname, '../json')));
 // To correctly receive the json data
 app.use(express.json({ limit: '1mb' }));
 
-let firstTime = true;
-let locations = [];
+const database = new Datastore('database.db');
+// load if existed or create new 
+database.loadDatabase();
 
 app.post('/api', (request, response) =>{ 
-
-    // Read for the first time
-    const stats = fs.statSync('locations.json');
-    if(firstTime){
-        // read location from file
-        fs.readFile('locations.json', (err, read_data) =>{
-            if(err){
-                console.log(err);
-                return;
-            }
-            json = stats["size"] == 0? [] : JSON.parse(read_data);
-            locations = json;
-        });
-        firstTime = false;
-    }
-
     const data = request.body;
-    locations.push(data);
-    
+    const timestamp = Date.now();
+    data.timestamp = timestamp;
+    database.insert(data);
     response.json({
        status: "success",
-       latitude: data.latitude,
-       longitude: data.longitude
+       lat: data.lat,
+       lon: data.lon,
+       timestamp: data.timestamp,
+       mood: data.mood
    });
-
-   appendLog('locations.json', data);
-});
-
-function appendLog(path, data){
-    const stats = fs.statSync(path);
-    fs.readFile(path, (err, read_data) =>{
-        if(err){
-            console.log(err);
-            return;
-        }
-        
-        const json = stats["size"] == 0 ? [] : JSON.parse(read_data);
-        json.push({
-            'latitude':data.latitude,
-            'longitude': data.longitude
-        });
-        
-        fs.writeFile(path, JSON.stringify(json), (err) =>{
-                if(err){
-                    console.log(err);
-                    return;
-                }
-                console.log('Successfully writed into file!');
-            });
    
-    });
-}
+});
